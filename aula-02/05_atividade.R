4a10bd4a1b7738c8e852f5003ca1c3b36b01785b
Ithon <ithondewes1@gmail.com>
2018-04-24 01:21:28 +0000

    teste

--- aula-02/05_atividade.R
+++ aula-02/05_atividade.R
@@ -12,11 +12,12 @@ load("aula-02/data/dados_exercicio.RData")
 ##         quero ver uma saída na Console.
 ### # ####
 
-
+str(acessos_alunos)
 
 ### 2 ###
 ## Quantos elementos a variável acessos_alunos possui? Utilize uma função do R que retorna o tamanho da variável.
 
+length(acessos_alunos)
 ## Dica: Vimos um exemplo no mesmo material sobre estruturas de dados
 ### # ###
 
